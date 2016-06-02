d93804f106d447b505ba7c667300add0cd8da4fd
Peng Li <pli@MacBook-Pro.local>
2016-06-02 12:04:41 +0800

    O(n) using HashMap

---
+++ 1. Two Sum/two_sum.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by pli on 6/2/16.
+ */
+public class two_sum {
+
+    public class Solution {
+        public int[] twoSum(int[] nums, int target) {
+
+            Map<Integer, Integer> map = new HashMap<>();
+
+            for (int i = 0; i < nums.length; i++) {
+
+                if (map.containsKey(target - nums[i])) {
+                    return new int[] {map.get(target -nums[i]), i};
+                } else {
+                    map.put(nums[i], i);
+                }
+            }
+
+            throw new IllegalArgumentException("No two sum solution");
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
