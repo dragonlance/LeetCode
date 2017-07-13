0f43ba57f0a767ddf947792175481471ee0c9b9c
Peng Li <pengli@email.com>
2017-07-12 21:49:03 -0700

    Add new solution for problem 16 3Sum Closest

--- 0016_3Sum_Closest.java
+++ 0016_3Sum_Closest.java
@@ -33,3 +33,40 @@ public class Solution {
         return closestSum;
     }
 }
+
+// 另一种经典的求极值的写法
+public class Solution2 {
+    public int threeSumClosest(int[] nums, int target) {
+
+        // 另一种常用的找极值的方法
+        // 先用一个全局变量，存一个实际的初始值，而不是MAX_VALUE之类的
+        int closestSum = nums[0] + nums[1] + nums[2];
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // 这一步可以优化去重
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[left] + nums[right] + nums[i];
+                if (sum == target) return sum;
+                if (Math.abs(target - sum) < Math.abs(target - closestSum)) {
+                    closestSum = sum;
+                }
+                if (sum < target) {
+                    left++;
+                    // 去重优化，但是无法从指标上提升
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                } else if (sum > target) {
+                    right--;
+                    // 去重优化， 但是无法从指标上提升
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                }
+            }
+        }
+        return closestSum;
+    }
+}
