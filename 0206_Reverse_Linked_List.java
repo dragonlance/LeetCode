0550cb87c7067b72f4e75d69088ff28e7afb3e1e
Peng Li <pengli@email.com>
2017-08-05 18:45:38 -0700

    Add solutions to problem 206. Reverse Linked List

--- 0206_Reverse_Linked_List.java
+++ 0206_Reverse_Linked_List.java
@@ -20,3 +20,13 @@ public class Solution1 {
         return pre;
     }
 }
+public class Solution2 {
+    // Recursive
+    public static ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode tail = reverseList(head.next); //bottom up从最底层返回tail，穿越回最顶层
+        head.next.next = head;//右边指向左边
+        head.next = null;//目前是一个环，左边指向右边的指针置空
+        return tail;
+    }
+}
