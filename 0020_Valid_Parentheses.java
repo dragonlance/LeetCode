f748b37bf7adcf3c2dc17bec231af5f2001c1e2b
Peng Li <pengli@email.com>
2017-08-08 01:15:20 -0700

    Add solution for problem 20. Valid Parentheses

--- 0020_Valid_Parentheses.java
+++ 0020_Valid_Parentheses.java
@@ -1,4 +1,4 @@
-public class Solution {
+public class Solution1 {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack();
         for (int i = 0; i < s.length(); i++) {
@@ -7,5 +7,24 @@ public class Solution {
             else if (stack.empty() || c - stack.pop() > 2 ) return false;
         }
         return stack.empty();
+    }
+}
+
+public class Solution2 {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');// directly push right parenthesis if meet left
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (c == '{') {
+                stack.push('}');
+                // will throw StackEmptyException if not check stack if empty
+            } else if (stack.isEmpty() || c != stack.pop()) {
+                return false;
+            }
+        }
+        return stack.isEmpty(); // stack should be empty if all parenthesis are paired.
     }
 }
