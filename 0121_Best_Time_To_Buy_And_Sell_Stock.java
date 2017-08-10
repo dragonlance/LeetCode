6db5c1686aca1479f8042f2ba0a9fddbb47039cd
Peng Li <pengli@email.com>
2017-08-10 00:58:41 -0700

    Add comment to problem 121. Best Time to Buy and Sell Stock

--- 0121_Best_Time_To_Buy_And_Sell_Stock.java
+++ 0121_Best_Time_To_Buy_And_Sell_Stock.java
@@ -4,8 +4,8 @@ public class Solution {
         int min = Integer.MAX_VALUE;
         int profit = Integer.MIN_VALUE;
         for (int i : prices) {
-            min = Math.min(i, min);
-            profit = Math.max(i - min, profit);
+            min = Math.min(i, min);//把目前出现过的最小值存下来
+            profit = Math.max(i - min, profit);//然后用目前出现的所有值，减去目前的最小值
         }
         return profit;
     }
